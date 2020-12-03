deb7dca99412068a6c4469340a3532ac28cd2b04
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-12-03 13:40:47 +0000

    added custmoers by course name and passed test

--- src/test/java/com/codeclan/example/courseManager/CourseManagerApplicationTests.java
+++ src/test/java/com/codeclan/example/courseManager/CourseManagerApplicationTests.java
@@ -1,12 +1,18 @@
 package com.codeclan.example.courseManager;
 
+import com.codeclan.example.courseManager.models.Booking;
 import com.codeclan.example.courseManager.models.Course;
+import com.codeclan.example.courseManager.models.Customer;
+import com.codeclan.example.courseManager.repositories.BookingRepository;
 import com.codeclan.example.courseManager.repositories.CourseRepository;
+import com.codeclan.example.courseManager.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +27,12 @@ class CourseManagerApplicationTests {
 	@Autowired
 	CourseRepository courseRepository;
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
 	@Test
 	public void canFindCourseByRating(){
 		Course java = new Course("Intro To Java", "Inverness", 4);
@@ -28,5 +40,20 @@ class CourseManagerApplicationTests {
 		assertEquals("Intro To Java", courseRepository.findCourseByRating(4).get(0).getName());
 	}
 
+	@Test
+	public void canGetCustomersByCourse(){
+		Customer bob = new Customer("Bob", "Stirling", 36);
+		customerRepository.save(bob);
+		Customer jeannie = new Customer("Jeannie", "Falkirk", 25);
+		customerRepository.save(jeannie);
+		Course java = new Course("Intro To Java", "Inverness", 4);
+		courseRepository.save(java);
+		Booking booking = new Booking("12/21", java, bob);
+		bookingRepository.save(booking);
+		Booking booking1 = new Booking("12/21", java, jeannie);
+		bookingRepository.save(booking1);
+		List<Booking> results = bookingRepository.findBookingsByCourseName("Intro To Java");
+		assertEquals(2, results.size());
+	}
 
 }
