732c95f638939f4c7f99e11d6138ae338f02c518
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-12-03 15:19:14 +0000

    added more dataloader values

--- src/main/java/com/codeclan/example/courseManager/components/DataLoader.java
+++ src/main/java/com/codeclan/example/courseManager/components/DataLoader.java
@@ -28,17 +28,33 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args){
-        Course course1 = new Course("Intro To Java", "Inverness", 2);
-        courseRepository.save(course1);
-        Course java = new Course("", "Edinburgh", 4);
+        Course java = new Course("Intro To Java", "Inverness", 2);
         courseRepository.save(java);
-        Customer bob = new Customer("Bob", "Stirling", 36);
-        customerRepository.save(bob);
-        Customer jeannie = new Customer("Jeannie", "Falkirk", 25);
+        Course javaScript = new Course("Advanced Javascript", "Edinburgh", 4);
+        courseRepository.save(javaScript);
+        Course python = new Course("Hellish Python", "Glasgow", 3);
+        courseRepository.save(python);
+
+        Customer arnie = new Customer("Arnie", "Inverness", 36);
+        customerRepository.save(arnie);
+        Customer sally = new Customer("Sally", "Edinburgh", 25);
+        customerRepository.save(sally);
+        Customer chuck = new Customer("Chuck", "Glasgow", 45);
+        customerRepository.save(chuck);
+        Customer jeannie = new Customer("Jeannie", "Edinburgh", 28);
         customerRepository.save(jeannie);
-        Booking booking = new Booking("12/21", java, bob);
-        bookingRepository.save(booking);
-        Booking booking1 = new Booking("11/21", java, jeannie);
+        Customer bruce = new Customer("Bruce", "Glasgow", 32);
+        customerRepository.save(bruce);
+
+        Booking booking1 = new Booking("12/21", java, arnie);
         bookingRepository.save(booking1);
+        Booking booking2 = new Booking("11/21", java, jeannie);
+        bookingRepository.save(booking2);
+        Booking booking3 = new Booking("10/20", javaScript, sally);
+        bookingRepository.save(booking3);
+        Booking booking4 = new Booking("09/19", python, chuck);
+        bookingRepository.save(booking4);
+        Booking booking5 = new Booking("10/18", python, bruce);
+        bookingRepository.save(booking5);
     }
 }
