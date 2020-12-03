3893d2f20491637a7ed18a133dffff2411c4cd94
David Murphy <davidmurphy@Davids-MacBook-Pro.local>
2020-12-03 14:58:00 +0000

    Added data to DataLoader

--- src/main/java/com/codeclan/example/courseManager/components/DataLoader.java
+++ src/main/java/com/codeclan/example/courseManager/components/DataLoader.java
@@ -1,8 +1,11 @@
 package com.codeclan.example.courseManager.components;
 
+import com.codeclan.example.courseManager.models.Booking;
 import com.codeclan.example.courseManager.models.Course;
+import com.codeclan.example.courseManager.models.Customer;
+import com.codeclan.example.courseManager.repositories.BookingRepository;
 import com.codeclan.example.courseManager.repositories.CourseRepository;
-//import com.codeclan.example.courseManager.repositories.CustomerRepository;
+import com.codeclan.example.courseManager.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,18 +14,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationRunner {
 
-//    @Autowired
-//    CustomerRepository customerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
 
     @Autowired
     CourseRepository courseRepository;
+
+    @Autowired
+    BookingRepository bookingRepository;
 
     public DataLoader(){
 
     }
 
     public void run(ApplicationArguments args){
-        Course course = new Course("Intro To Java", "Inverness", 2);
-        courseRepository.save(course);
+        Course course1 = new Course("Intro To Java", "Inverness", 2);
+        courseRepository.save(course1);
+        Course java = new Course("", "Edinburgh", 4);
+        courseRepository.save(java);
+        Customer bob = new Customer("Bob", "Stirling", 36);
+        customerRepository.save(bob);
+        Customer jeannie = new Customer("Jeannie", "Falkirk", 25);
+        customerRepository.save(jeannie);
+        Booking booking = new Booking("12/21", java, bob);
+        bookingRepository.save(booking);
+        Booking booking1 = new Booking("11/21", java, jeannie);
+        bookingRepository.save(booking1);
     }
 }
